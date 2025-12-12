c0da04ceb96ca813281dd12b20edacc8ba6dc87e
Max Tasker <mmttasker@gmail.com>
2025-12-12 21:47:22 +0000

    is_constructor and is_field flags now set by declaration collector

--- steelc/src/ast_passes/declaration_collector.cpp
+++ steelc/src/ast_passes/declaration_collector.cpp
@@ -210,10 +210,11 @@ void declaration_collector::visit(std::shared_ptr<type_declaration> decl) {
 
 	current_type = decl;
 	for (const auto& member : decl->fields) {
+		member->is_field = true;
 		member->accept(*this);
 	}
 	for (const auto& constructor : decl->constructors) {
-		//constructor->is_constructor = true;
+		constructor->is_constructor = true;
 		constructor->accept(*this);
 	}
 	for (const auto& method : decl->methods) {
