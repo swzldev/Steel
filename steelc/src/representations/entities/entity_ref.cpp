6b31d96795ecfaeae644a4c4de90bde7ee758c17
Max Tasker <mmttasker@gmail.com>
2025-12-14 17:36:06 +0000

    entity ref impl

--- steelc/src/representations/entities/entity_ref.cpp
+++ steelc/src/representations/entities/entity_ref.cpp
@@ -1,1 +1,5 @@
 #include "entity_ref.h"
+
+entity_ptr entity_ref::get(const std::shared_ptr<symbol_table>& sym_table) const {
+	return sym_table->get_by_id(id);
+}
--- steelc/src/representations/entities/entity_ref.h
+++ steelc/src/representations/entities/entity_ref.h
@@ -3,6 +3,7 @@
 #include <memory>
 
 #include <representations/entities/entities_fwd.h>
+#include <symbolics/symbol_table.h>
 
 class entity_ref {
 public:
@@ -10,7 +11,7 @@ public:
 		: id(-1) {
 	}
 
-	entity_ptr get() const;
+	entity_ptr get(const std::shared_ptr<symbol_table>& sym_table) const;
 
 private:
 	entity_id id;
