1243a6b1b7de65d5206fd2da6c8c7d0911de4d51
Max Tasker <mmttasker@gmail.com>
2025-12-17 17:54:16 +0000

    remove module holder

--- steelc/src/codegen_llvm/llvm_code_generator.h
+++ steelc/src/codegen_llvm/llvm_code_generator.h
@@ -1,5 +1,8 @@
 #pragma once
 
-class llvm_code_generator {
+#include <codegen/icode_generator.h>
+#include <codegen/codegen_result.h>
 
+class llvm_code_generator : icode_generator {
+	codegen_result emit(const mir_module& module) override;
 };
