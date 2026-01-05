9503a8b6df4b541664eae70c7eef0f49e621ed0f
Max Tasker <mmttasker@gmail.com>
2026-01-05 19:57:58 +0000

    add utility methods, supported abis list, name and description, also removed output kind in place of just capabilities

--- steelc/src/codegen/codegen_info.h
+++ steelc/src/codegen/codegen_info.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 #include <sys/platform.h>
 
@@ -16,15 +17,33 @@ enum class codegen_capability {
 	CAN_GENERATE_NATIVE_OBJECT,
 };
 
-enum class codegen_output_kind {
-	NATIVE_OBJECT,
-};
-
 class codegen_info {
 public:
+	inline bool capable_of(codegen_capability cap) const {
+		return contains(capabilities, cap);
+	}
+	inline bool supports_arch(platform_arch arch) const {
+		return contains(supported_architectures, arch);
+	}
+	inline bool supports_os(platform_os os) const {
+		return contains(supported_oses, os);
+	}
+	inline bool supports_abi(platform_abi abi) const {
+		return contains(supported_abis, abi);
+	}
+
+	std::string name;
+	std::string description;
+
 	std::vector<codegen_capability> capabilities;
-	std::vector<codegen_output_kind> output_kinds;
 
 	std::vector<platform_arch> supported_architectures;
 	std::vector<platform_os> supported_oses;
+	std::vector<platform_abi> supported_abis;
+
+private:
+	template<typename T>
+	inline bool contains(const std::vector<T>& vec, T val) const {
+		return std::find(vec.begin(), vec.end(), val) != vec.end();
+	}
 };
